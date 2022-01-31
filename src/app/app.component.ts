4cddea3bbc8b44413d5dec6dda72d29f6acef062
Austin Campbell <austincamp@gmail.com>
2022-01-31 08:45:18 -0500

    initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ac-pipes';
+  convertText: string = "I Am Being Managed By Pipes";
+  extrasolarPlanets: any = [
+    {'name': 'CoRoT-7b'}, 
+    {'name': 'Gliese 581'}, 
+    {'name': 'HD 209458b'}, 
+    {'name': 'HIP 13044b'}, 
+    {'name': 'Kepler-186f'}, 
+    {'name': 'Kepler-452b'}
+  ];
+
+// add somethign here
 }
